415e3b95a04e068341778f9151e27fa9370fbfa0
spectator05 <spectat@kookmin.ac.kr>
2022-06-27 11:27:30 +0900

    init : lpbyf class

---
+++ lpbyf/lpbyf.py
@@ -0,0 +1,18 @@
+import os
+
+class lpbyf:
+    def __init__(self):
+        pass
+
+    # Make label dict by using flow(csv)
+        # parameter : <path : string, 5-tuple-index : each column?>  
+
+        # self.label_dict
+
+# other class
+
+    # Pcap Reader type(line by line)
+        # parameter : none
+
+    # Pcap Store in RAM type
+        # parameter : none
